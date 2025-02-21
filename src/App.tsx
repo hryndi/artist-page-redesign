f0aae66c1d270a99300e042932f06f586497f121
Hrynov <Dmytro.Hrynov@tof.de>
2025-02-21 14:46:22 +0100

    started header

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,48 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className=" parent relative font-Marcellus ">
+        <section>
+          <div>
+            <header className="z-50 absolute w-full flex flex-col items-center justify-center py-5 pt-16 gap-5  ">
+              <span className=" font-Silkscreen text-6xl text-white">
+                some logo here
+              </span>
+              <ul className="flex gap-8 nav text-2xl text-gray-100">
+                <li>
+                  <a className=" "> Home </a>
+                </li>
+                <li>
+                  <a> About us </a>
+                </li>
+                <li>
+                  <a> Gallery </a>
+                </li>
+                <li>
+                  <a> Albums </a>
+                </li>
+                <li>
+                  <a> Contacts </a>
+                </li>
+              </ul>
+            </header>
+          </div>
+          {/* Background wrapper */}
+          <div className="relative h-screen bg-no-repeat bg-cover bg-center">
+            {/* Dark overlay */}
+            <div className="absolute inset-0 z-20 bg-black opacity-35"></div>
+
+            {/* Background image */}
+            <div
+              className="absolute inset-0 bg-no-repeat bg-cover bg-center"
+              style={{ backgroundImage: "url(/man-7795464_1920.jpg)" }}
+            ></div>
+          </div>
+        </section>
+        <section className="h-screen custom-gradient"></section>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
