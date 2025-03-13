337d0149b90f066334fe464936bd945b7173ff63
Hrynov <Dmytro.Hrynov@tof.de>
2025-03-13 09:32:56 +0100

    removed unused values 2

--- src/components/hero.tsx
+++ src/components/hero.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+// import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
 import { ArrowDown, Music2 } from "lucide-react";
 import { Button } from "./ui/button";
